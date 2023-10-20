68bcb6e0d04855ac43e808732c30bd73ebb0478f
Dario <dar.rua.car@gmail.com>
2023-10-20 17:29:02 +0200

    helloworld_app added [FLutter]

---
+++ M08/helloworld_app/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(MaterialApp(
+      home: Scaffold(
+    appBar: AppBar(
+      title: const Text('Hello World'),
+      centerTitle: true,
+    ),
+    body: const Center(
+      child: Text('Dario Rua - M08 - S2AM 2023-2024'),
+    ),
+  )));
+}
