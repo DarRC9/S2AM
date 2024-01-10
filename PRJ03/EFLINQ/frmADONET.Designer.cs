e9bfcd9543b4da4f0ee9f023cdda599126db82e3
Dario <dar.rua.car@gmail.com>
2024-01-10 17:15:56 +0100

    LINQ

---
+++ PRJ03/EFLINQ/frmADONET.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace EFLINQ
+{
+    partial class frmADONET
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.TableDgv = new System.Windows.Forms.DataGridView();
+            this.NewBtn = new System.Windows.Forms.Button();
+            this.UpdateBtn = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.OpenEDIBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.TableDgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 49);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 77);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 2;
+            // 
+            // TableDgv
+            // 
+            this.TableDgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.TableDgv.Location = new System.Drawing.Point(12, 126);
+            this.TableDgv.Name = "TableDgv";
+            this.TableDgv.RowHeadersWidth = 51;
+            this.TableDgv.RowTemplate.Height = 24;
+            this.TableDgv.Size = new System.Drawing.Size(776, 254);
+            this.TableDgv.TabIndex = 3;
+            // 
+            // NewBtn
+            // 
+            this.NewBtn.Location = new System.Drawing.Point(12, 398);
+            this.NewBtn.Name = "NewBtn";
+            this.NewBtn.Size = new System.Drawing.Size(75, 40);
+            this.NewBtn.TabIndex = 4;
+            this.NewBtn.Text = "New";
+            this.NewBtn.UseVisualStyleBackColor = true;
+            // 
+            // UpdateBtn
+            // 
+            this.UpdateBtn.Location = new System.Drawing.Point(108, 398);
+            this.UpdateBtn.Name = "UpdateBtn";
+            this.UpdateBtn.Size = new System.Drawing.Size(75, 40);
+            this.UpdateBtn.TabIndex = 5;
+            this.UpdateBtn.Text = "Update";
+            this.UpdateBtn.UseVisualStyleBackColor = true;
+            this.UpdateBtn.Click += new System.EventHandler(this.UpdateBtn_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "OpenEdiOfd";
+            // 
+            // OpenEDIBtn
+            // 
+            this.OpenEDIBtn.Location = new System.Drawing.Point(561, 40);
+            this.OpenEDIBtn.Name = "OpenEDIBtn";
+            this.OpenEDIBtn.Size = new System.Drawing.Size(93, 30);
+            this.OpenEDIBtn.TabIndex = 6;
+            this.OpenEDIBtn.Text = "Open .EDI";
+            this.OpenEDIBtn.UseVisualStyleBackColor = true;
+            this.OpenEDIBtn.Click += new System.EventHandler(this.OpenEDIBtn_Click);
+            // 
+            // frmADONET
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.OpenEDIBtn);
+            this.Controls.Add(this.UpdateBtn);
+            this.Controls.Add(this.NewBtn);
+            this.Controls.Add(this.TableDgv);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "frmADONET";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmADONET_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.TableDgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DataGridView TableDgv;
+        private System.Windows.Forms.Button NewBtn;
+        private System.Windows.Forms.Button UpdateBtn;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button OpenEDIBtn;
+    }
+}
+
---
+++ PRJ03/EFLINQ/frmADONET.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace EFLINQ
+{
+    public partial class frmADONET : Form
+    {
+        public frmADONET()
+        {
+            InitializeComponent();
+        }
+
+        ClientsEntities db = new ClientsEntities();
+        List<NewContact> newCon;
+        List<ContactSystem> conSys;
+
+        public NewContact _currentContact;
+        
+        private void frmADONET_Load(object sender, EventArgs e)
+        {
+            //LoadData();
+        } 
+
+        private void LoadData()
+        {
+            conSys = (from c in db.ContactSystem select c).ToList();
+            newCon = db.NewContact.ToList();
+
+            TableDgv.DataSource = conSys;
+        }
+
+        private void openEdiFile()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "EDI Files|*.edi|All Files|*.*";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+                ProcessEdiFile(filePath);
+            }
+        }
+
+        private void ProcessEdiFile(string filePath)
+        {
+            List<string> ediLines = File.ReadAllLines(filePath).ToList();
+
+            ediLines.ForEach(ProcessEdiLine);
+        }
+
+        private void ProcessEdiLine(string line)
+        {
+            string[] values = line.Split('|');
+            string recordType = values[0];
+
+            if (recordType == "CON") { 
+
+                string name = values[1];
+                string dob = values[2];
+                string phone = values[3];
+
+                NewContact newContact = new NewContact
+                {
+                    Name = name,
+                    BirthDate = dob,
+
+                };
+
+                db.NewContact.Add(newContact);
+                db.SaveChanges();
+
+                NewContact currentContact = db.NewContact.FirstOrDefault(c => c.Name == name && c.BirthDate == dob);
+                _currentContact = currentContact;
+
+                ContactSystem contactSystem = new ContactSystem
+                {
+                    IdContact = _currentContact.idContact,
+                    Description = "Mobile",
+                    SystemValue = phone,
+                };
+
+                db.ContactSystem.Add(contactSystem);
+                db.SaveChanges();
+
+        } else if (recordType == "SYS")
+            {
+                string desc = values[1];
+                string value = values[2];
+
+                ContactSystem contactSystem = new ContactSystem
+                {
+                    IdContact = _currentContact.idContact,
+                    Description = desc,
+                    SystemValue = value,
+                };
+
+                db.ContactSystem.Add(contactSystem);
+                db.SaveChanges();
+            }
+        }
+
+        private void OpenEDIBtn_Click(object sender, EventArgs e)
+        {
+            openEdiFile();
+        }
+
+        private void UpdateBtn_Click(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+    }
+}
